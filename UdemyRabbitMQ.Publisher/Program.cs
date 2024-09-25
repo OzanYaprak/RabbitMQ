271ce024c8a856be998b9aced81bc71cda43a3db
Ozan YAPRAK <ozanerhanyaprak@icloud.com>
2024-09-25 14:23:21 +0300

    Add project files.

---
+++ UdemyRabbitMQ.Publisher/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace UdemyRabbitMQ.Publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory();
+
+            connectionFactory.Uri = new Uri("amqps://cvdsmplc:rPV4bmv5QDAJXd8oe-25QULUW9BdA-wT@rat.rmq2.cloudamqp.com/cvdsmplc");
+
+
+            using var connection = connectionFactory.CreateConnection();
+            
+            
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("hello-queue", true, false, false);
+
+            string message = "hello world";
+            var messageBody = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+            Console.WriteLine("Mesaj Gönderilmiştir.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UdemyRabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,13 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace UdemyRabbitMQ.Subscriber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
